5628ebd691eccdfab606b1f34c424f3d41a0ae2b
Ubuntu <ubuntu@ip-172-31-35-157.ap-southeast-2.compute.internal>
2022-03-02 06:28:02 +0000

    Added files for templates and created base html

--- application/__init__.py
+++ application/__init__.py
@@ -8,8 +8,9 @@ def create_app():
     app.config["SECRET_KEY"] = "some secret string"
 
     from .views import views
+    from .auth import auth
 
     app.register_blueprint(views, url_prefix="/")
-    auth.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
 
     return app
