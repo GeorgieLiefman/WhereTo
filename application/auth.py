1da6151f38776c4f01344defdabb666212ee7d91
Ubuntu <ubuntu@ip-172-31-35-157.ap-southeast-2.compute.internal>
2022-03-04 11:26:22 +0000

    Able to signup and login users

--- application/auth.py
+++ application/auth.py
@@ -1,16 +1,28 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from . import db
 from .models import User
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint("auth", __name__)
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
-    email = request.form.get("email")
-    password = request.form.get("password")
-    
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in!", category="success")
+                login_user(user, remember=True)
+                return redirect(url_for("views.home"))
+            else:
+                flash("Password is incorrect", category="error")
+        else:
+            flash("Email is not registered to a user.", category="error")  
+
     return render_template("login.html")
 
 @auth.route("/sign_up", methods=["GET", "POST"])
@@ -38,7 +50,8 @@ def sign_up():
         else:
             new_user = User(email=email, username=username, password=generate_password_hash(password1, method="sha256"))
             db.session.add(new_user)
-            db.sesssion.commit()
+            db.session.commit()
+            login_user(new_user, remember=True)
             flash('User created!')
             return redirect(url_for("views.home"))
 
@@ -46,5 +59,7 @@ def sign_up():
     return render_template("signup.html")
 
 @auth.route("/log_out", methods=["GET"])
+@login_required
 def log_out():
+    logout_user()
     return redirect(url_for("views.home"))
