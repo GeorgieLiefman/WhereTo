c6180fbaa1dd99fc88de98fc75bcb37e29c6fa33
Ubuntu <ubuntu@ip-172-31-35-157.ap-southeast-2.compute.internal>
2022-03-04 07:34:09 +0000

    Added data inputs on views from form

--- application/auth.py
+++ application/auth.py
@@ -4,12 +4,18 @@ auth = Blueprint("auth", __name__)
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    
     return render_template("login.html")
 
 @auth.route("/sign_up", methods=["GET", "POST"])
 def sign_up():
+    email = request.form.get("email")
     username = request.form.get("username")
-    print(username)
+    password1 = request.form.get("password1")
+    password2 = request.form.get("password2")
+    
     return render_template("signup.html")
 
 @auth.route("/log_out", methods=["GET"])
