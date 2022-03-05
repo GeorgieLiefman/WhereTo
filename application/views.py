aa1c5dd2c36400b1cd1ae6b0e25946443e0e7bf4
Ubuntu <ubuntu@ip-172-31-35-157.ap-southeast-2.compute.internal>
2022-03-05 01:41:15 +0000

    Created route for creating a review

--- application/views.py
+++ application/views.py
@@ -8,3 +8,9 @@ views = Blueprint("views", __name__)
 @login_required
 def home():
     return render_template("home.html", name=current_user.username, user=current_user)
+
+
+@views.route("/create_review", methods=["GET", "POST"])
+@login_required
+def create_review():
+    return render_template("create_review.html", user=current_user)
