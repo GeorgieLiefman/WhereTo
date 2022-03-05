e752d5d9f7a960e7752dbb6bc6e43a5c5899d751
Ubuntu <ubuntu@ip-172-31-35-157.ap-southeast-2.compute.internal>
2022-03-05 07:04:52 +0000

    Created functionality to submit reviews and appropiate html

--- application/views.py
+++ application/views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
+from .models import Review
+from . import db
 
 views = Blueprint("views", __name__)
 
@@ -7,10 +9,23 @@ views = Blueprint("views", __name__)
 @views.route("/")
 @login_required
 def home():
-    return render_template("home.html", name=current_user.username, user=current_user)
+    reviews = Review.query.all()
+    return render_template("home.html", name=current_user.username, user=current_user, reviews=reviews)
 
 
 @views.route("/create_review", methods=["GET", "POST"])
 @login_required
 def create_review():
+    if request.method == "POST":
+        text = request.form.get("text")
+
+        if not text:
+            flash("Review cannot be empty", category="error")
+        else:
+            review = Review(text=text, author=current_user.id)
+            db.session.add(review)
+            db.session.commit()
+            flash("Review created!", category="successful")
+            return redirect(url_for("views.home"))
+
     return render_template("create_review.html", user=current_user)
