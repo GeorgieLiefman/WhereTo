44345022c0dee1ecb61a182b3b7c39aab9ce7f04
Ubuntu <ubuntu@ip-172-31-35-157.ap-southeast-2.compute.internal>
2022-03-05 11:06:21 +0000

    Added functionality to delete comments

--- application/views.py
+++ application/views.py
@@ -75,5 +75,22 @@ def create_comment(review_id):
         else:
             flash("Review does not exist.", category="error")
 
-    
+
+    return redirect(url_for("views.home"))
+
+
+@views.route("/delete_comment/<comment_id>", methods=["GET"])
+@login_required
+def delete_comment(comment_id):
+    comment = Comment.query.filter_by(id=comment_id).first()
+
+    if not comment:
+        flash("Comment does not exist.", category="error")
+    elif current_user.id != comment.author and current_user.id != comment.review.author:
+        flash("You do not have permission to delete this comment.", category="error")
+    else:
+        db.session.delete(comment)
+        db.session.commit()
+        
+
     return redirect(url_for("views.home"))
