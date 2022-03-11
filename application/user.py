8146603eb049751464c84951e6fb6fa5abffa139
GeorgieLiefman <gkb.liefman@gmail.com>
2022-03-12 02:37:17 +1100

    Added docstrings and comments to user.py

--- application/user.py
+++ application/user.py
@@ -4,10 +4,23 @@ from .models import User
 from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 
-user = Blueprint("auth", __name__)
+# Create a blueprint named user which will be used for routes relating to users and authentication.
+user = Blueprint("user", __name__)
 
+# Create a route for the login page.
 @user.route("/login", methods=["GET", "POST"])
 def login():
+    """
+    This function allows users to both access the login page and creates the necessary functionalities 
+    to login into their accounts.
+
+    Parameters:
+        GET:/login 
+        POST:/login
+
+    Returns:
+        It renders the login.html template and redirects users to the homepage if they sign up.
+    """
     if request.method == "POST":
         email = request.form.get("email")
         password = request.form.get("password")
@@ -25,8 +38,21 @@ def login():
 
     return render_template("login.html", user=current_user)
 
+
+# Creates a route to access the sign up page and its functionalities.
 @user.route("/sign_up", methods=["GET", "POST"])
 def sign_up():
+    """
+    This function allows users to both access the sign up page and creates the necessary functionalities 
+    to create a new account.
+
+    Parameters:
+        GET:/sign_up 
+        POST:/sign_up
+
+    Returns:
+        It renders the signup.html template and redirects users to the homepage if they sign up.
+    """
     if request.method == "POST":
         email = request.form.get("email")
         username = request.form.get("username")
@@ -36,15 +62,15 @@ def sign_up():
         email_exists = User.query.filter_by(email=email).first()
         username_exists = User.query.filter_by(username=username).first()
         if email_exists:
-            flash("Email is already in use.", category="error")
+            flash("Email is already registered to an account.", category="error")
         elif username_exists:
-            flash("Username is already in use.", category="error")
+            flash("Username is already registered to an account.", category="error")
         elif password1 != password2:
-            flash("Password does not match.", category="error")
+            flash("The passwords do not match.", category="error")
         elif len(username) < 4:
-            flash("Username is too short.", category="error")
+            flash("Username is too short, usernames need to be 4 characters or greater.", category="error")
         elif len(password1) < 8:
-            flash("Password is too short.", category="error")
+            flash("Password is too short, passwords need to be 8 characters or greater.", category="error")
         elif len(email) < 5:
             flash("Email is invalid.", category="error")
         else:
@@ -52,14 +78,25 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
-            flash('User created!')
+            flash('Successfully created and logged in user.')
             return redirect(url_for("views.home"))
 
     
     return render_template("signup.html", user=current_user)
 
+# Creates a route to allow users to logout.
 @user.route("/log_out", methods=["GET"])
+# Users have to be logged in to access this functionality.
 @login_required
 def log_out():
+    """
+    This function allows users sign out of their account.
+
+    Parameters:
+        GET:/log_out 
+
+    Returns:
+        It redirects users to the homepage if they choose to log out.
+    """
     logout_user()
     return redirect(url_for("views.home"))
