59a785f591909746603096898e7d2498b1142f7a
Ubuntu <ubuntu@ip-172-31-35-157.ap-southeast-2.compute.internal>
2022-03-11 15:15:40 +0000

    Updated views to reflect changes in models

--- application/views.py
+++ application/views.py
@@ -17,15 +17,27 @@ def home():
 @login_required
 def create_review():
     if request.method == "POST":
-        text = request.form.get("text")
+        title = request.form.get("title")
+        content = request.form.get("content")
+        destination = request.form.get("destination")
+        category = request.form.get("category")
+        price = request.form.get("price")
 
-        if not text:
-            flash("Review cannot be empty", category="error")
+        if not title:
+            flash("A review cannot be created without a title.", category="error")
+        elif not content:
+            flash("The content of a review cannot be empty.", category="error")
+        elif not destination:
+            flash("A destination needs to be entered to create a review.", category="error")
+        elif not category:
+            flash("Reviews cannot be created without the category of review being specified.", category="error")
+        elif not price:
+            flash("Reviews cannot be created without the price of review being specified. If the experience was free please specify so.", category="error")
         else:
-            review = Review(text=text, author=current_user.id)
+            review = Review(title=title, content=content, destination=destination, category=category, price=price, creator=current_user.id)
             db.session.add(review)
             db.session.commit()
-            flash("Review created!", category="successful")
+            flash("Review successfully created!", category="successful")
             return redirect(url_for("views.home"))
 
     return render_template("create_review.html", user=current_user)
@@ -37,15 +49,14 @@ def delete_review(id):
     review = Review.query.filter_by(id=id).first()
 
     if not review:
-        flash("Review does not exist.", category="error")
-    elif current_user.id != review.id:
-        flash("You do not have permission to delete this post.", category="error")
+        flash("The review you are trying to delete does not exist.", category="error")
     else:
         db.session.delete(review)
         db.session.commit()
-        flash("Review deleted.", category="success")
+        flash("The review has been successfully deleted.", category="success")
 
     return redirect(url_for("views.home"))
+
 
 @views.route("/reviews/<username>", methods=["GET"])
 @login_required
@@ -53,28 +64,27 @@ def reviews(username):
     user = User.query.filter_by(username=username).first()
 
     if not user:
-        flash("No user with that username exists.", category="error")
+        flash("No user with that username is registered.", category="error")
         return redirect(url_for("views.home"))
 
-    reviews = Review.query.filter_by(author=user.id).all()
+    reviews = Review.query.filter_by(creator=user.id).all()
     return render_template("reviews.html", user=current_user, reviews=reviews, username=username)
 
 
 @views.route("/create_comment/<review_id>", methods=["POST"])
 @login_required
 def create_comment(review_id):
-    text = request.form.get("text")
-    if not text:
-        flash("Comment cannot be empty.", category="error")
+    content = request.form.get("content")
+    if not content:
+        flash("The content of a comment cannot be empty.", category="error")
     else:
         review = Review.query.filter_by(id=review_id)
         if review:
-            comment = Comment(text=text, author=current_user.id, review_id=review_id)
+            comment = Comment(content=content, creator=current_user.id, review_id=review_id)
             db.session.add(comment)
             db.session.commit()
         else:
-            flash("Review does not exist.", category="error")
-
+            flash("The review you are trying to comment on does not exist.", category="error")
 
     return redirect(url_for("views.home"))
 
@@ -85,12 +95,14 @@ def delete_comment(comment_id):
     comment = Comment.query.filter_by(id=comment_id).first()
 
     if not comment:
-        flash("Comment does not exist.", category="error")
-    elif current_user.id != comment.author and current_user.id != comment.review.author:
+        flash("The comment you are trying to delete does not exist.", category="error")
+    elif current_user.id != comment.creator and current_user.id != comment.review.creator:
         flash("You do not have permission to delete this comment.", category="error")
     else:
         db.session.delete(comment)
         db.session.commit()
+        
+    return redirect(url_for("views.home"))
         
 
     return redirect(url_for("views.home"))
@@ -98,19 +110,18 @@ def delete_comment(comment_id):
 
 @views.route("/like_review/<review_id>", methods=["GET"])
 @login_required
-def like(review_id):
+def heart(review_id):
     review = Review.query.filter_by(id=review_id)
-    like = Like.query.filter_by(author=current_user.id, review_id=review_id).first()
-
+    heart = Heart.query.filter_by(creator=current_user.id, review_id=review_id).first()
 
     if not review:
-        flash("Review does not exist.", category="error")
-    elif like:
-        db.session.delete(like)
+        flash("The review you are trying to heart does not exist.", category="error")
+    elif heart:
+        db.session.delete(heart)
         db.session.commit()
     else:
-        like = Like(author=current_user.id, review_id=review_id)
-        db.session.add(like)
+        heart = Heart(creator=current_user.id, review_id=review_id)
+        db.session.add(heart)
         db.session.commit() 
 
     return redirect(url_for("views.home"))
